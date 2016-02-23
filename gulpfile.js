cb9d0a6633e63fa1990ac4f878dc3a72ae9b0361
Stewart Yu <stewartyu@gmail.com>
2016-02-22 19:36:13 -0800

    use flexbox for column component

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./public/css/*.scss', ['sass']);
+  gulp.watch('./public/css/**/*.scss', ['sass']);
 });
 
 gulp.task('develop', function () {
